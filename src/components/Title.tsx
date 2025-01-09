09ccdc32a79c4afd87e026fb75e44f7381716afb
wolicodes <676095+wolicodes@users.noreply.github.com>
2025-01-09 13:56:37 +0100

    first screen

---
+++ src/components/Title.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./Title.module.css";
+
+const Title: React.FC = () => {
+  return <h1 className={classes.title}>Ootdle</h1>;
+};
+
+export default Title;
