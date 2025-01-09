64222c91b80c14e08da7c8702a5863fd73f382f6
wolicodes <676095+wolicodes@users.noreply.github.com>
2025-01-09 13:56:37 +0100

    adjustments

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./reset.css";
 import "./index.css";
+import { BrowserRouter } from "react-router";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
